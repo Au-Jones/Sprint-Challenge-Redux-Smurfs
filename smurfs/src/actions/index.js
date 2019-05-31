f8b60ceb5f222fd1ca3cba8fb87ca6a8fdbf16e6
Your Name <aujones95@gmail.com>
2019-05-31 11:55:39 -0400

    finished the actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,3 +1,39 @@
+import axios from 'axios'
+export const FETCHING_SUCCESS = 'FETCHING_SUCCESS';
+export const FETCHING = 'FETCHING';
+export const FETCHING_FAILURE = 'FETCHING_FAILURE';
+export const ADDING = 'ADDING';
+export const ADDED = 'ADDED';
+export const ERROR = 'ERROR';
+
+export const fetchSmurfs = () => dispatch => {
+  dispatch({ type: FETCHING})
+  axios
+  .get(`http://localhost:3333/smurfs`)
+  .then(res => {
+    dispatch({ type: FETCHING_SUCCESS, payload: res.data})
+  })
+  .catch(err => {
+    dispatch({ type: FETCHING_FAILURE, payload: err})
+  });
+};
+
+export const addNewSmurf = (newSmurf) => dispatch => {
+  dispatch({ type: ADDING });
+  axios
+  .post('http://localhost:3333/smurfs', newSmurf)
+  .then(res => {
+    dispatch({ type: ADDED, payload: res.data});
+  })
+  .catch(err => {
+    dispatch({ type:ERROR, payload: err})
+  });
+}
+
+
+
+
+
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
